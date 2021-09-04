6500acb53d29e4623ffa985d5293db7107f9c1ad
Minhthang1506 <19522216@gm.uit.edu.vn>
2021-09-04 19:31:45 +0700

    .

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-module.exports = (req, res) => {
-    res.json({
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
-    });
-  };
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({
+    extended: true,
+}))
+
+app.get('/about', (req, res) => res.send('About Page Route'));
+
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
