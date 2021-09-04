a1469ee1d25280022f4176c91357d8504749d028
Minhthang1506 <19522216@gm.uit.edu.vn>
2021-09-04 19:14:19 +0700

    .

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-const { response } = require('express');
+
 const express = require('express');
 const app = express();
-const swaggerUi = require('swagger-ui-express');
 app.use(express.urlencoded({
     extended: true,
 }))
 
-var options = {
-    swaggerOptions: {
-      url: 'http://petstore.swagger.io/v2/swagger.json'
-    }
-}
+app.get('/about', (req, res) => res.send('About Page Route'));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options), (req, res) => {
-    console.log(req.body)
-});
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-
-//app.get('/about', (req, res) => res.send('About Page Route'));
-
-//app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-//app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
 const port = process.env.PORT || 3000;
 
